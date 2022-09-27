5f3f8ae7eac7ac4703dae9e445a55ab0255874b7
TingLiu <rryuutei@gmail.com>
2022-09-26 21:45:19 -0700

    Assignment 2

---
+++ BasicTableView/BasicTableView/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  BasicTableView
+//
+//  Created by Ting Liu on 9/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // Adding comment
+    }
+    
+    
+
+
+}
+
